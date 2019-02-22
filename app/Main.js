8b5357450538e52caaec9f4874078a664448e13e
Sanusi Yusuf Korede <sanusikorede@gmail.com>
2019-02-22 11:14:38 +0100

    add button component

--- app/Main.js
+++ app/Main.js
@@ -1,23 +1,128 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar, ScrollView } from 'react-native';
+import { StyleSheet, View, StatusBar, ActivityIndicator, ScrollView, AsyncStorage } from 'react-native';
 import { LinearGradient } from 'expo';
+import uuid from 'uuid/v1';
 import { primaryGradientArray } from './utils/Colors'; 
 import Header from './components/Header';
+// import SubTitle from './components/SubTitle';
 import Input from './components/Input';
 import List from './components/List';
-
+// import Button from './components/Button';
 
 const headerTitle = 'To Do';
 
 export default class Main extends React.Component {
     state = {
-        inputValue: ''
+        inputValue: '',
+        loadingItems: false,
+        allItems: {},
+        isCompleted: false
+    };
+    componentDidMount = () => {
+        this.loadingItems();
     };
     newInputValue = value => {
         this.setState({
             inputValue: value
         });
     };
+    loadingItems = async () => {
+        try {
+            const allItems = await AsyncStorage.getItem('ToDos');
+            this.setState({
+                loadingItems: true,
+                allItems: JSON.parse(allItems) || {}
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    };
+    onDoneAddItem = () => {
+        const { inputValue } = this.state;
+        if (inputValue !== '') {
+            this.setState(prevState => {
+                const id = uuid();
+                const newItemObject = {
+                    [id]: {
+                        id,
+                        isCompleted: false,
+                        text: inputValue,
+                        createdAt: Date.now()
+                    }
+                };
+                const newState = {
+                    ...prevState,
+                    inputValue: '',
+                    allItems: {
+                        ...prevState.allItems,
+                        ...newItemObject
+                    }
+                };
+                this.saveItems(newState.allItems);
+                return { ...newState };
+            });
+        }
+    };
+    deleteItem = id => {
+        this.setState(prevState => {
+            const allItems = prevState.alignItems;
+            delete allItems[id];
+            const newState = {
+                ...prevState,
+                ...allItems
+            };
+            this.saveItems(newState.allItems);
+            return { ...newState };
+        });
+    };
+
+    
+
+    completeItem = id => {
+        this.setState(prevState => {
+          const newState = {
+            ...prevState,
+            allItems: {
+              ...prevState.allItems,
+              [id]: {
+                ...prevState.allItems[id],
+                isCompleted: true
+              }
+            }
+          };
+          this.saveItems(newState.allItems);
+          return { ...newState };
+        });
+      };
+      incompleteItem = id => {
+        this.setState(prevState => {
+          const newState = {
+            ...prevState,
+            allItems: {
+              ...prevState.allItems,
+              [id]: {
+                ...prevState.allItems[id],
+                isCompleted: false
+              }
+            }
+          };
+          this.saveItems(newState.allItems);
+          return { ...newState };
+        });
+      };
+      deleteAllItems = async () => {
+        try {
+          await AsyncStorage.removeItem('ToDos');
+          this.setState({ allItems: {} });
+        } catch (err) {
+          console.log(err);
+        }
+      };
+      saveItems = newItem => {
+        const saveItem = AsyncStorage.setItem('To Dos', JSON.stringify(newItem));
+      };
+
+
     render() {
         const { inputValue } = this.state;
         return (
@@ -33,7 +138,7 @@ export default class Main extends React.Component {
 
                 <View style={styles.list}>
                     <ScrollView contentContainerStyle={styles.scrollableList}>
-                        {Object.values(allItems)
+                        {Object.values({allItems})
                             .reverse()
                             .map(item => (
                                 <List
